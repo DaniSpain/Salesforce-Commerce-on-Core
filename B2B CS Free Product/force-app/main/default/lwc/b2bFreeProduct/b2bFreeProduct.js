22a298fd460e938911e887eb2246eacabda9c55d
DaniSpain <danielespagnuolo89@gmail.com>
2024-12-17 17:44:30 +0100

    Added ability to add manual discount

--- B2B CS Free Product/force-app/main/default/lwc/b2bFreeProduct/b2bFreeProduct.js
+++ B2B CS Free Product/force-app/main/default/lwc/b2bFreeProduct/b2bFreeProduct.js
@@ -1,16 +1,20 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 import { publish, subscribe, MessageContext } from 'lightning/messageService';
 
 import COMMERCE_CART_CHANGED from '@salesforce/messageChannel/lightning__commerce_cartChanged';
 
 import UserId from '@salesforce/user/Id';
+import communityId from '@salesforce/community/Id';
+
+import { CartSummaryAdapter, refreshCartSummary } from 'commerce/cartApi';
 
 import getCartItems from '@salesforce/apex/B2BFreeProductController.getCartItems';
 import setFree from '@salesforce/apex/B2BFreeProductController.setFree';
+import applyPercentageDiscount from '@salesforce/apex/B2BFreeProductController.applyPercentageDiscount';
+import applyDiscount from '@salesforce/apex/B2BFreeProductController.applyDiscount';
 import isLoggedOnBehalf from '@salesforce/apex/B2BFreeProductController.isLoggedOnBehalf';
 import getCurrentSession from '@salesforce/apex/B2BFreeProductController.getCurrentSession';
 
-//TODO: visualizzare solo se utente sottostante è un CS
 //TODO: aggiungere uno spinner
 
 export default class B2bFreeProduct extends LightningElement {
@@ -21,6 +25,18 @@ export default class B2bFreeProduct extends LightningElement {
     isLoginOnBehalf;
     showSpinner = true;
     cartItems;
+    @track currencyIsoCode = "EUR";
+    @track cartSummary;
+    
+    showModal = false;
+    showModalSpinner = false;
+    curCartItemId;
+    @track discountAmount;
+    discountTypes = [
+        { label: 'Percent', value: 'percent' },
+        { label: 'Amount', value: 'amount' }
+    ];
+    @track discountType = 'percent';
 
     @wire(MessageContext)
     messageContext;
@@ -33,7 +49,23 @@ export default class B2bFreeProduct extends LightningElement {
             COMMERCE_CART_CHANGED,
             (message) => this.getItems()
         );
-        this.init();
+        //this.init();
+    }
+
+    @wire(CartSummaryAdapter)
+    cartSummaryHandler(response) {
+        if (response.data) {
+            console.log("B2bFreeProduct::Got data from cart adapter");
+            console.log(response.data);
+            this.cartSummary = response.data;
+            this.cartId = response.data.cartId;
+            this.effectiveAccountId = response.data.accountId;
+            this.currencyIsoCode = response.data.currencyIsoCode;
+            this.init();
+        } else if (response.error) {
+            console.error("B2bFreeProduct::Error getting data from cart adapter");
+            console.error(response.error);
+        }
     }
 
     disconnectedCallback() {
@@ -42,6 +74,9 @@ export default class B2bFreeProduct extends LightningElement {
 
     init() {
         this.showSpinner = true;
+        /*
+        * Commentato per ora per test fatto con utenza partner community
+        *
         getCurrentSession({})
         .then(sessionInfo => {
             console.log("B2bFreeProduct::init::getLoggedAsDetails::SUCCESS");
@@ -53,6 +88,9 @@ export default class B2bFreeProduct extends LightningElement {
             console.error("B2bFreeProduct::init::getLoggedAsDetails::ERROR");
             console.error(error);
         })
+        */
+        this.isLoginOnBehalf = true;
+        this.getItems();
     }
 
     getItems() {
@@ -81,10 +119,53 @@ export default class B2bFreeProduct extends LightningElement {
             console.log("B2bFreeProduct::connectedCallback::setFree::SUCCESS");
             publish(this.messageContext, COMMERCE_CART_CHANGED);
             this.showSpinner = false;
+            refreshCartSummary();
         }).catch(error => {
             console.error("B2bFreeProduct::connectedCallback::setFree::ERROR");
             console.error(error);
         });
+    }
+
+    openApplyDiscountModal(event) {
+        console.log("Cart Item ID: " + event.currentTarget.dataset.id);
+        this.curCartItemId = event.currentTarget.dataset.id;
+        this.showModal = true;
+    }
+
+    //TODO: provare a gestire il caso di sconto su totale carrello
+    invokeApplyDiscount() {
+        console.log("B2bFreeProduct::applyDiscount::appying discount = " + this.discountAmount + " to cartItemId = " + this.curCartItemId);
+        applyDiscount({
+            cartItemId: this.curCartItemId,
+            discount: this.discountAmount,
+            discountType: this.discountType,
+            currencyIsoCode: this.currencyIsoCode
+        }).then(result => {
+            console.log("B2bFreeProduct::applyDiscount::SUCCESS");
+            publish(this.messageContext, COMMERCE_CART_CHANGED);
+            refreshCartSummary();
+            this.showModalSpinner = false;
+            this.showModal = false;
+        }).catch(error => {
+            console.error("B2bFreeProduct::applyDiscount::ERROR");
+            console.error(error);
+            this.showModalSpinner = false;
+            this.showModal = false;
+        });
+    }
+
+    closeModal() {
+        this.showModal = false;
+    }
+
+    handleInputChange(event) {
+        this.discountAmount = event.detail.value;
+    }
+
+    handleDiscountTypeChange(event) {
+        console.log("Discount Type Changed");
+        console.log(event.detail.value);
+        this.discountType = event.detail.value;
     }
 
     getCartItemById(cartItemId) {
